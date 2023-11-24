f6bfedc6f0d7f9bf0c1a2fab3d979fc4df9d4304
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-11-24 12:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+const btn = document.getElementById('button');
+const url = document.getElementById('url');
+const name = document.getElementById('name');
+const year = document.getElementById('year');
+// adding event listener
+btn.addEventListener("click", () => {
+	if (name.value && year.value) {
+		url.innerText = `https://localhost:8080/?name=${name.value}&year=${year.value}`;
+	}
+	else if (name.value){
+		url.innerText = `https://localhost:8080/?name=${name.value}`;	
+	}
+	else if(year.value){
+		url.innerText = `https://localhost:8080/?year=${year.value}`;
+	}
+	else{
+		url.innerText = `https://localhost:8080/111`;
+	}
+})
